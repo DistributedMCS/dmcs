b791966fa4d9ea8b2166e340dc5cc29bb08f4957
tkren <tkren@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2010-04-27 06:30:53 +0000

    Resuscitate testParsers testcase.

--- examples/dia-4-4-2-2-1_command_line.sh
+++ examples/dia-4-4-2-2-1_command_line.sh
@@ -2,9 +2,9 @@
 export TIMEFORMAT=$'\nreal\t%3R\nuser\t%3U\nsys\t%3S'
 export TESTSPATH='tests'
 export DMCSPATH='.'
-$DMCSPATH/dmcsd 1 localhost 5001 $TESTSPATH/dia-4-4-2-2-1-1.lp $TESTSPATH/dia-4-4-2-2-1-1.br $TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
-$DMCSPATH/dmcsd 2 localhost 5002 $TESTSPATH/dia-4-4-2-2-1-2.lp $TESTSPATH/dia-4-4-2-2-1-2.br $TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
-$DMCSPATH/dmcsd 3 localhost 5003 $TESTSPATH/dia-4-4-2-2-1-3.lp $TESTSPATH/dia-4-4-2-2-1-3.br $TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
-$DMCSPATH/dmcsd 4 localhost 5004 $TESTSPATH/dia-4-4-2-2-1-4.lp $TESTSPATH/dia-4-4-2-2-1-4.br $TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
-time $DMCSPATH/dmcsc localhost 5001 4 $TESTSPATH/dia-4-4-2-2-1.mv 
+$DMCSPATH/dmcsd --context=1 --port=5001 --kb=$TESTSPATH/dia-4-4-2-2-1-1.lp --br=$TESTSPATH/dia-4-4-2-2-1-1.br --topology=$TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
+$DMCSPATH/dmcsd --context=2 --port=5002 --kb=$TESTSPATH/dia-4-4-2-2-1-2.lp --br=$TESTSPATH/dia-4-4-2-2-1-2.br --topology=$TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
+$DMCSPATH/dmcsd --context=3 --port=5003 --kb=$TESTSPATH/dia-4-4-2-2-1-3.lp --br=$TESTSPATH/dia-4-4-2-2-1-3.br --topology=$TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
+$DMCSPATH/dmcsd --context=4 --port=5004 --kb=$TESTSPATH/dia-4-4-2-2-1-4.lp --br=$TESTSPATH/dia-4-4-2-2-1-4.br --topology=$TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
+time $DMCSPATH/dmcsc --hostname=localhost --port=5001 --system-size=4 --query-variables="18446744073709551615 1 1 1 "
 killall dmcsd
--- src/tests/testParsers.cpp
+++ src/tests/testParsers.cpp
@@ -1,11 +1,10 @@
-#include "Theory.h"
-#include "CnfGrammar.h"
-#include "CnfBuilder.h"
-#include "ClaspResultGrammar.h"
-#include "ClaspResultBuilder.h"
 #include "ParserDirector.h"
-#include "DimacsVisitor.h"
 #include "BeliefState.h"
+#include "Rule.h"
+#include "Signature.h"
+#include "QueryPlan.h"
+#include "LocalKBBuilder.h"
+#include "PropositionalASPGrammar.h"
 
 #include <algorithm>
 #include <cstdlib>
@@ -25,103 +24,38 @@
 using namespace dmcs;
 
 
-TheoryPtr theory(new Theory);
-BiMapId2Name id2name;
-BiMapId2Lid id2lid;
-BiMapId2Lid my_id2lid;
-
-
-void
-create_local_id()
-{
-  int i = 1;
-  for (BiMapId2Name::left_const_iterator it = id2name.left.begin(); 
-       it != id2name.left.end(); ++i, ++it)
-    {
-      BiMapId2Lid::value_type p(it->first, i);
-      id2lid.insert(p);
-    }
-}
-
-// void
-// print_theory()
-// {
-//   std::cerr << "Encoding atoms:" << std::endl;
-
-//   for (BiMapId2Name::left_const_iterator it1 = id2name.left.begin(); it1 != id2name.left.end(); ++it1)
-//     {
-//       std::cerr << it1->second << " --> "
-// 		<< it1->first << " --> "
-// 		<< id2lid.left.at(it1->first)
-// 		<< std::endl;
-//     }
-
-//   std::cerr << std::endl;
-
-//   std::cerr << "The theory: " << std::endl;
-  
-//   for (Theory::const_iterator it2 = theory->begin(); it2 != theory->end(); ++it2)
-//     {
-//       ClausePtr c = *it2;
-//       for (Clause::const_iterator it3 = c->begin(); it3 != c->end(); ++it3)
-//        	{
-//        	  std::cerr << *it3 << ", ";
-//        	}
-//       std::cerr << std::endl;
-//     }
-
-//   std::cerr << std::endl << "Dimacs style: " << std::endl;
-
-//   std::ostringstream s;
-//   DimacsVisitor dv(s);
-//   dv.visitTheory(theory, id2lid);
-//   std::cerr << s.str() << std::endl;
-// }
-
-// void print_clasp_result()
-// {
-//   std::cerr << std::endl << "Belief sets parsed from clasp result:" << std::endl;
-//   for (BeliefSets::const_iterator it = belief_sets->begin(); it != belief_sets->end(); ++it)
-//     {
-//       BeliefSet bs = *it;
-//       std::cerr << "{";
-//       for (std::map<int, char>::const_iterator it2 = bs.begin(); it2 != bs.end(); ++it2)
-// 	{
-// 	  std::cerr << it2->first << it2->second << ", ";
-// 	}
-//       std::cerr << "}" << std::endl;
-//     }
-// }
-
-
-
-BOOST_AUTO_TEST_CASE( testParsersCnf )
+BOOST_AUTO_TEST_CASE( testParsersKB )
 {
   const char* ex = getenv("EXAMPLESDIR");
+
+  assert (ex != 0);
+
+  std::size_t myid = 1;
+
   // some theory file
-  std::string theoryfile(ex);
-  theoryfile += "/sat1.txt";
-  // some clasp results file
-  std::string resultfile(ex);
-  resultfile += "/clasp-result.txt";
+  std::string filename_local_kb(ex);
+  filename_local_kb += "/dia-4-4-2-2-1-1.lp";
 
+  std::string filename_topo(ex);
+  filename_topo += "/dia-4-4-2-2-1.opt";
 
-  CnfBuilder<CnfGrammar> builder(theory, id2name);
-  ParserDirector<CnfGrammar> parser_director;
+  QueryPlanPtr query_plan(new QueryPlan);
 
+  query_plan->read_graph(filename_topo);
+
+  SignaturePtr sig(new Signature);
+  *sig = query_plan->getSignature(myid);
+  std::size_t system_size = query_plan->getSystemSize();
+
+  RulesPtr local_kb(new Rules);
+
+  LocalKBBuilder<PropositionalASPGrammar> builder(local_kb, sig);
+  ParserDirector<PropositionalASPGrammar> parser_director;
   parser_director.setBuilder(&builder);
-  parser_director.parse(theoryfile);
+  parser_director.parse(filename_local_kb);
+}
 
-  BiMapId2Lid sat_id2lid = builder.getLocalSATId();
-  
-  Neighbors nbs;
-  std::size_t system_size = 1;
 
-  Context context(1, theory, id2name, sat_id2lid, my_id2lid, system_size, nbs, true);
-  BeliefStatesPtr bs(new BeliefStates(system_size));
-  ClaspResultBuilder<ClaspResultGrammar> builder2(context, bs);
-  ParserDirector<ClaspResultGrammar> parser_director2;
-  parser_director2.setBuilder(&builder2);
-  parser_director2.parse(resultfile);
-  // print_clasp_result();
+BOOST_AUTO_TEST_CASE( testParsersBR )
+{
 }
