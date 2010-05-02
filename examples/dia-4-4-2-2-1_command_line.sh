79345db5f0b8c72994e950295f2f3445482d3d2b
tkren <tkren@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2010-05-02 09:18:28 +0000

    Fix V for basic DMCS.

--- examples/dia-4-4-2-2-1_command_line.sh
+++ examples/dia-4-4-2-2-1_command_line.sh
@@ -6,5 +6,5 @@ $DMCSPATH/dmcsd --context=1 --port=5001 --kb=$TESTSPATH/dia-4-4-2-2-1-1.lp --br=
 $DMCSPATH/dmcsd --context=2 --port=5002 --kb=$TESTSPATH/dia-4-4-2-2-1-2.lp --br=$TESTSPATH/dia-4-4-2-2-1-2.br --topology=$TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
 $DMCSPATH/dmcsd --context=3 --port=5003 --kb=$TESTSPATH/dia-4-4-2-2-1-3.lp --br=$TESTSPATH/dia-4-4-2-2-1-3.br --topology=$TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
 $DMCSPATH/dmcsd --context=4 --port=5004 --kb=$TESTSPATH/dia-4-4-2-2-1-4.lp --br=$TESTSPATH/dia-4-4-2-2-1-4.br --topology=$TESTSPATH/dia-4-4-2-2-1.top >/dev/null 2>&1 &
-time $DMCSPATH/dmcsc --hostname=localhost --port=5001 --system-size=4 --query-variables="18446744073709551615 1 1 1 "
+time $DMCSPATH/dmcsc --hostname=localhost --port=5001 --system-size=4 --query-variables="18446744073709551615 17 13 23 "
 killall dmcsd
