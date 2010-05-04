c0f39ede165839939f3411b1224102eab6e342f7
bairakdar <bairakdar@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2010-05-04 18:19:14 +0000

    Update queryplan computation.

--- include/HouseOptQPGenerator.h
+++ include/HouseOptQPGenerator.h
@@ -36,108 +36,137 @@
 
 namespace dmcs { namespace generator {
 
-class HouseOptQPGenerator : public HouseQPGenerator
-{
-public:
-  HouseOptQPGenerator(ContextsPtr& contexts_, QueryPlanPtr& query_plan_)
-    : HouseQPGenerator(contexts_, query_plan_)
-  { }
+    class HouseOptQPGenerator : public HouseQPGenerator
+    {
+    public:
+      HouseOptQPGenerator(ContextsPtr& contexts_, QueryPlanPtr& query_plan_)
+	: HouseQPGenerator(contexts_, query_plan_)
+      { }
 
-  void
-  create_interface(const ContextPtr& context)
-  { 
+      void
+      create_interface(const ContextPtr& context)
+      { 
 
-    std::size_t id = context->getContextID();
+	std::size_t id = context->getContextID();
 
-    if (id == 1)
-      {
-	  BeliefStatePtr bs_12 = query_plan->getInterface1(id, id+1);
+	if (id == 1)
+	  {
+#ifdef DEBUG
+	      std::cerr << "root case "<<std::endl;
+#endif
+	    BeliefStatePtr bs_12 = query_plan->getInterface1(id, id+1);	  
+	    const BeliefStatePtr bs_23 = query_plan->getInterface1(id+1, id+2);
+
+	    update(bs_12, bs_23);
+	    query_plan->putInterface(id, id+1, bs_12);
+#ifdef DEBUG
+	      std::cerr << "done with root case"<<std::endl;
+#endif
+	    return;
+	  }
+
+	int remainder = id % 4;
+
+	switch (remainder)
+	  {
+	  case 2:
+	    {
 	  
-	  const BeliefStatePtr bs_23 = query_plan->getInterface1(id+1, id+2);
-	  update(bs_12, bs_23);
-	  query_plan->putInterface(id, id+1, bs_12);
-	return;
-      }
-
-    int remainder = id % 4;
-
-    switch (remainder)
-      {
-      case 2:
-	{
-	  std::size_t houseId = (id+2)/4;
-	  std::size_t parentId = 1;
-	  if(houseId % 2 == 0)
-	    {
-	      parentId = 2*(((id-2)/4)+1);
-	    }
-	  else
-	    {
-	      if(id != 2)
+#ifdef DEBUG
+	      std::cerr << "In case 2"<<std::endl;
+#endif
+	      std::size_t houseId = (id+2)/4;
+	      std::size_t parentId = 1;
+	      if(houseId % 2 == 0)
 		{
-		  parentId = 2*(((id-5)/4)+1) +1;
+		  parentId = 2*(((id-2)/4)+1);
 		}
-	    }
-	  BeliefStatePtr bs_23 = query_plan->getInterface1(id, id+1);
-	   const BeliefStatePtr bs_p3 = query_plan->getInterface1(parentId, id+1);
-	  //	  const BeliefStatePtr bs_30 = query_plan->getInterface(id+1, id+2);
-	  const BeliefStatePtr bs_12 = query_plan->getInterface1(id+3, id);
+	      else
+		{
+		  if(id != 2)
+		    {
+		      parentId = 2*(((id-5)/4)+1) +1;
+		    }
+		}
+	      BeliefStatePtr bs_23 = query_plan->getInterface1(id, id+1);
+	      const BeliefStatePtr bs_p3 = query_plan->getInterface1(parentId, id+1);
+	      const BeliefStatePtr bs_30 = query_plan->getInterface1(id+1, id+2);
 
-	  update(bs_23, bs_p3);
-	  update(bs_23, bs_12);
-	  query_plan->putInterface(id, id+1, bs_23);
-	  query_plan->remove_connection(id+3, id);
-	  query_plan->remove_connection(parentId, id+1);
-	}
-	break;
-      case 3:
-	{
-	  BeliefStatePtr bs_30 = query_plan->getInterface1(id, id+1);
-
-	  const BeliefStatePtr bs_12 = query_plan->getInterface1(id+2, id-1);	  
-	  //	  const BeliefStatePtr bs_01 = query_plan->getInterface(id+1, id+2);
-	  
-	  update(bs_30, bs_12);
-	  query_plan->putInterface(id, id+1, bs_30);
-	}
-	break;
-      case 0:
-	{
-	  BeliefStatePtr bs_01 = query_plan->getInterface1(id, id+1);	  
-	  const BeliefStatePtr bs_12 = query_plan->getInterface1(id+1, id-2);
-	  update(bs_01, bs_12);
-	  query_plan->putInterface(id, id+1, bs_01);
-	  //	  query_plan->remove_connection(id+1, id-2);
-	  
-	  std::size_t childId = id + ((id/4)-1)*4 + 2;
-	  if(childId < context->getSystemSize())
-	    {
-	      BeliefStatePtr bs_p2 = query_plan->getInterface1(id, childId);
-	      
-	      const BeliefStatePtr bs_23 = query_plan->getInterface1(childId, childId+1);
-	      
-	      update(bs_p2, bs_23);
-	      query_plan->putInterface(id, childId, bs_p2);
+	      update(bs_23, bs_p3);
+	      update(bs_23, bs_30);
+	      query_plan->putInterface(id, id+1, bs_23);
+	      query_plan->remove_connection(parentId, id+1);
+#ifdef DEBUG
+	      std::cerr << "doen with case 2"<<std::endl;
+#endif
 	    }
-	}
-	break;
-      case 1:
-	{
-	  std::size_t childId = id + ((id/4)-1)*4 + 5;
-	  if(childId < context->getSystemSize())
+	    break;
+	  case 3:
 	    {
-	      BeliefStatePtr bs_p2 = query_plan->getInterface1(id, childId);
-	      
-	      const BeliefStatePtr bs_23 = query_plan->getInterface1(childId, childId+1);
-	      
-	      update(bs_p2, bs_23);
-	      query_plan->putInterface(id, childId, bs_p2);
+#ifdef DEBUG
+	      std::cerr << "In case 3"<<std::endl;
+#endif
+	      BeliefStatePtr bs_30 = query_plan->getInterface1(id, id+1);
+	      std::cerr << "1 " << *bs_30 << std::endl;
+	      const BeliefStatePtr bs_01 = query_plan->getInterface1(id+1, id+2);
+	      std::cerr << "2" << *bs_01 << std::endl;	  
+	      update(bs_30, bs_01);
+	      std::cerr << "3" <<std::endl;
+	      query_plan->putInterface(id, id+1, bs_30);
+#ifdef DEBUG
+	      std::cerr << "done with case 3"<<std::endl;
+#endif
 	    }
-	}
-	break;
+	    break;
+	  case 0:
+	    {
+#ifdef DEBUG
+	      std::cerr << "In case 0"<<std::endl;
+#endif
+	      BeliefStatePtr bs_01 = query_plan->getInterface1(id, id+1);	  
+	      const BeliefStatePtr bs_12 = query_plan->getInterface1(id+1, id-2);
+	      update(bs_01, bs_12);
+	      query_plan->putInterface(id, id+1, bs_01);
+	      query_plan->remove_connection(id+1, id-2);
+	  
+	      std::size_t childId = id + ((id/4)-1)*4 + 2;
+	      if(childId < context->getSystemSize())
+		{
+		  BeliefStatePtr bs_p2 = query_plan->getInterface1(id, childId);
+	      
+		  const BeliefStatePtr bs_23 = query_plan->getInterface1(childId, childId+1);
+	      
+		  update(bs_p2, bs_23);
+		  query_plan->putInterface(id, childId, bs_p2);
+		}
+#ifdef DEBUG
+	      std::cerr << "done with case 0"<<std::endl;
+#endif
+	    }
+	    break;
+	  case 1:
+	    {
+#ifdef DEBUG
+	      std::cerr << "In case 1"<<std::endl;
+#endif
+	      std::size_t childId = id + ((id/4)-1)*4 + 5;
+	      if(childId < context->getSystemSize())
+		{
+		  BeliefStatePtr bs_p2 = query_plan->getInterface1(id, childId);
+	      
+		  const BeliefStatePtr bs_23 = query_plan->getInterface1(childId, childId+1);
+	      
+		  update(bs_p2, bs_23);
+		  query_plan->putInterface(id, childId, bs_p2);
+		}
+#ifdef DEBUG
+	      std::cerr << "done with case 1"<<std::endl;
+#endif
+	    }
+	    break;
+	  }
       }
-  }
-};
+    };
 
 
   } // namespace generator
