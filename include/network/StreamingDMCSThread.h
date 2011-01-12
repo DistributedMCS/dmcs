665cf894f6ddf66a924b6659ed4ef05a35319597
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2011-01-12 17:38:39 +0000

    New file.

---
+++ include/network/StreamingDMCSThread.h
@@ -0,0 +1,62 @@
+/* DMCS -- Distributed Nonmonotonic Multi-Context Systems.
+ * Copyright (C) 2009, 2010 Minh Dao-Tran, Thomas Krennwallner
+ * 
+ * This file is part of DMCS.
+ *
+ *  DMCS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DMCS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DMCS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   StreamingDMCSThread.h
+ * @author Minh Dao Tran <dao@kr.tuwien.ac.at>
+ * @date   Wed Jan  12 16:48:24 2011
+ * 
+ * @brief  
+ * 
+ * 
+ */
+
+#ifndef STREAMING_DMCS_THREAD_H
+#define STREAMING_DMCS_THREAD_H
+
+#include "dmcs/StreamingCommandType.h"
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
+
+namespace dmcs {
+
+class StreamingDMCSThread
+{
+public:
+  StreamingDMCSThread(const StreamingCommandTypePtr& scmt_);
+
+  void
+  operator()();
+
+private:
+  const StreamingCommandTypePtr scmt;
+};
+
+typedef boost::shared_ptr<StreamingDMCSThread> StreamingDMCSThreadPtr;
+
+} // namespace dmcs
+
+
+#endif // STREAMING_DMCS_THREAD_H
+
+// Local Variables:
+// mode: C++
+// End:
---
+++ src/network/StreamingDMCSThread.cpp
@@ -0,0 +1,48 @@
+/* DMCS -- Distributed Nonmonotonic Multi-Context Systems.
+ * Copyright (C) 2009, 2010 Minh Dao-Tran, Thomas Krennwallner
+ * 
+ * This file is part of DMCS.
+ *
+ *  DMCS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DMCS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DMCS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   StreamingDMCSThread.cpp
+ * @author Minh Dao Tran <dao@kr.tuwien.ac.at>
+ * @date   Wed Jan  12 16:48:59 2011
+ * 
+ * @brief  
+ * 
+ * 
+ */
+
+#include "network/StreamingDMCSThread.h"
+
+namespace dmcs {
+
+StreamingDMCSThread::StreamingDMCSThread(const StreamingCommandTypePtr& scmt_)
+  : scmt(scmt_)
+{ }
+
+void
+StreamingDMCSThread::operator()()
+{
+  scmt->execute();
+}
+
+} // namespace dmcs
+
+// Local Variables:
+// mode: C++
+// End:
