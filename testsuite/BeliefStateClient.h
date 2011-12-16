888cf6a67556581a35f0a4a4fc774008397da699
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2011-12-16 13:19:06 +0000

    Add copyright info.

--- testsuite/BeliefStateClient.h
+++ testsuite/BeliefStateClient.h
@@ -1,3 +1,33 @@
+/* DMCS -- Distributed Nonmonotonic Multi-Context Systems.
+ * Copyright (C) 2009, 2010 Minh Dao-Tran, Thomas Krennwallner
+ * 
+ * This file is part of DMCS.
+ *
+ *  DMCS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DMCS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DMCS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   BeliefStateClient.h
+ * @author Minh Dao-Tran <dao@kr.tuwien.ac.at>
+ * @date   Fri Dec  16 10:35:30 2011
+ * 
+ * @brief  
+ * 
+ * 
+ */
+
+
 #ifndef BELIEF_STATE_CLIENT_H
 #define BELIEF_STATE_CLIENT_H
 
