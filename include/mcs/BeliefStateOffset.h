c0d482bf258a8ae869b33b9862775d655720a5a8
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2011-12-25 16:00:56 +0000

    New files.

---
+++ include/mcs/BeliefStateOffset.h
@@ -0,0 +1,68 @@
+/* DMCS -- Distributed Nonmonotonic Multi-Context Systems.
+ * Copyright (C) 2009, 2010 Minh Dao-Tran, Thomas Krennwallner
+ * 
+ * This file is part of DMCS.
+ *
+ *  DMCS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DMCS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DMCS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   BeliefStateOffset.h
+ * @author Minh Dao Tran <dao@kr.tuwien.ac.at>
+ * @date   Sun Dec  25 16:34:24 2011
+ * 
+ * @brief  
+ * 
+ * 
+ */
+
+#ifndef BELIEF_STATE_OFFSET_H
+#define BELIEF_STATE_OFFSET_H
+
+#include "mcs/NewBeliefState.h"
+
+namespace dmcs {
+
+class BeliefStateOffset {
+
+public:
+  static BeliefStateOffset* 
+  create(std::size_t no_bs, std::size_t size_bs);
+
+  static BeliefStateOffset*
+  instance();
+
+  std::vector<BitMagic*>&
+  getMasks();
+
+  std::vector<std::size_t>&
+  getStartingOffsets();
+
+protected:
+  BeliefStateOffset(std::size_t no_bs, std::size_t size_bs);
+
+private:
+  std::vector<BitMagic*> masks;
+  std::vector<std::size_t> starting_offsets;
+
+  static BeliefStateOffset* _instance;
+};
+
+} // namespace dmcs
+
+#endif // BELIEF_STATE_OFFSET_H
+
+// Local Variables:
+// mode: C++
+// End:
