277f3a40a095f41836bf7b5ff4b40b5fcc49ff36
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2012-03-02 16:49:08 +0000

    NeighborOut thread.

---
+++ src/network/NewNeighborOut.cpp
@@ -0,0 +1,74 @@
+/* DMC -- Distributed Nonmonotonic Multi-Context Systems.
+ * Copyright (C) 2009, 2010 Minh Dao-Tran, Thomas Krennwallner
+ * 
+ * This file is part of DMCS.
+ *
+ *  DMCS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DMCS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DMCS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   NewNeighborOut.h
+ * @author Minh Dao Tran <dao@kr.tuwien.ac.at>
+ * @date   Fri Mar  2 9:23:26 2012
+ * 
+ * @brief  
+ * 
+ * 
+ */
+
+#include "mcs/ForwardMessage.h"
+#include "mcs/QueryID.h"
+#include "network/NewNeighborOut.h"
+
+namespace dmcs {
+
+NewNeighborOut::NewNeighborOut()
+{ }
+
+
+
+NewNeighborOut::~NewNeighborOut()
+{ }
+
+
+
+void
+NewNeighborOut::operator()(connection_ptr conn,
+			   NewConcurrentMessageDispatcherPtr md,
+			   std::size_t neighbor_offset)
+{
+  int timeout = 0;
+  while (1)
+    {
+      ForwardMessage* fwd_mess = md->receive<ForwardMessage>(NewConcurrentMessageDispatcher::NEIGHBOR_OUT_MQ, neighbor_offset, timeout);
+
+      conn->write(*fwd_mess);
+
+      if (shutdown(fwd_mess->query_id))
+	{
+	  delete fwd_mess;
+	  fwd_mess = 0;
+	  break;
+	}
+      
+      delete fwd_mess;
+      fwd_mess = 0;
+    }
+}
+
+} // namespace dmcs
+
+// Local Variables:
+// mode: C++
+// End:
