944b47ddffb453af44f3e3cc1096837c82bc39d1
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2012-04-26 17:14:39 +0000

    Working on the new test case.

--- testsuite/testSystem.cpp
+++ testsuite/testSystem.cpp
@@ -350,6 +350,8 @@ BOOST_AUTO_TEST_CASE ( testDiamondPlusSystem )
   std::size_t ctx_port4 = 7004;
   std::size_t ctx_port5 = 7005;
 
+  std::size_t pack_size = 5;
+
   /************************** CONTEXT 5 **************************/
   BeliefTablePtr btab55(new BeliefTable);
 
@@ -420,6 +422,14 @@ BOOST_AUTO_TEST_CASE ( testDiamondPlusSystem )
   ID id_dprimec3c6 = btab44->storeAndGetID(belief_dprimec3c6_44);
 
   BeliefTablePtr btab54(new BeliefTable);
+  Belief belief_epsilon54(ctx_id5, "epsilon");
+  Belief belief_ec1c4_54(ctx_id5, "e(c1,c4)");
+  Belief belief_ec2c5_54(ctx_id5, "e(c2,c5)");
+  Belief belief_ec3c6_54(ctx_id5, "e(c3,c6)");
+  Belief belief_fc1c4_54(ctx_id5, "f(c1,c4)");
+  Belief belief_fc2c5_54(ctx_id5, "f(c2,c5)");
+  Belief belief_fc3c6_54(ctx_id5, "f(c3,c6)");
+
   ID id_epsilon54 = ID(ID::MAINKIND_BELIEF | ctx_id5, 0);
   ID id_ec1c4_54 = ID(ID::MAINKIND_BELIEF | ctx_id5, 4);
   ID id_ec2c5_54 = ID(ID::MAINKIND_BELIEF | ctx_id5, 5);
@@ -428,7 +438,7 @@ BOOST_AUTO_TEST_CASE ( testDiamondPlusSystem )
   ID id_fc2c5_54 = ID(ID::MAINKIND_BELIEF | ctx_id5, 8);
   ID id_fc3c6_54 = ID(ID::MAINKIND_BELIEF | ctx_id5, 9);
 
-  BOOST_CHECK_EQUAL(id_epsilon54, id_epsilon_55);
+  BOOST_CHECK_EQUAL(id_epsilon54, id_epsilon55);
   BOOST_CHECK_EQUAL(id_ec1c4_54, id_ec1c4_55);
   BOOST_CHECK_EQUAL(id_ec2c5_54, id_ec2c5_55);
   BOOST_CHECK_EQUAL(id_ec3c6_54, id_ec3c6_55);
@@ -436,7 +446,126 @@ BOOST_AUTO_TEST_CASE ( testDiamondPlusSystem )
   BOOST_CHECK_EQUAL(id_fc2c5_54, id_fc2c5_55);
   BOOST_CHECK_EQUAL(id_fc3c6_54, id_fc3c6_55);
   
-  // add to btab54 using the new method
+  // add beliefs to btab54 using the new method
+  btab54->storeWithID(belief_epsilon54, id_epsilon54);
+  btab54->storeWithID(belief_ec1c4_54, id_ec1c4_54);
+  btab54->storeWithID(belief_ec2c5_54, id_ec2c5_54);
+  btab54->storeWithID(belief_ec3c6_54, id_ec3c6_54);
+  btab54->storeWithID(belief_fc1c4_54, id_fc1c4_54);
+  btab54->storeWithID(belief_fc2c5_54, id_fc2c5_54);
+  btab54->storeWithID(belief_fc3c6_54, id_fc3c6_54);
+
+  // bridge rules C5 --> C4
+  BridgeRuleTablePtr bridge_rules4(new BridgeRuleTable);
+
+  // adding all ground instances of d(X,Y) :- (5:f(X,Y)).
+  //
+  // d(c1,c4) :- (5:f(c1,c4)).
+  Tuple body14;
+  body14.push_back(id_fc1c4_54);
+  BridgeRule br14(ID::MAINKIND_RULE | ID::SUBKIND_RULE_BRIDGE_RULE, id_dc1c4_54, body14);
+  bridge_rules4->storeAndGetID(br14);
+
+  // d(c2,c5) :- (5:f(c2,c5)).
+  Tuple body24;
+  body24.push_back(id_fc2c5_54);
+  BridgeRule br24(ID::MAINKIND_RULE | ID::SUBKIND_RULE_BRIDGE_RULE, id_dc2c5_54, body24);
+  bridge_rules4->storeAndGetID(br24);
+  
+  // d(c3,c6) :- (5:f(c3,c6)).
+  Tuple body34;
+  body34.push_back(id_fc3c6_54);
+  BridgeRule br34(ID::MAINKIND_RULE | ID::SUBKIND_RULE_BRIDGE_RULE, id_dc3c6_54, body34);
+  bridge_rules4->storeAndGetID(br34);
+
+  std::size_t ctx_off54 = 0;
+  NewNeighborPtr neighbor54(new NewNeighbor(ctx_id5, ctx_off54, ctx_hostname5, port5));
+  NewNeighborVecPtr neighbors4(new NewNeighborVec);
+  neighbors4->push_back(neighbor54);
+
+  EnginePtr dlv_engine4 = DLVEngine::create();
+  EngineWPtr dlv_engine_wp4(dlv_engine4);
+  InstantiatorPtr dlv_inst4 = dlv_engine4->createInstantiator(dlv_engine_wp4, kbspec4);
+
+  NewContextPtr ctx4(new NewContext(ctx_id4, pack_size, dlv_inst4, btab4, bridge_rules4, neighbors4));
+  NewContextVecPtr contexts4(new NewContextVec);
+  contexts4->push_back(ctx4);
+
+  RegistryPtr reg4(new Registry(SYSTEM_SIZE, QUEUE_SIZE, BS_SIZE, contexts5));
+  boost::thread server_thread4(run_server, ctx_port4, reg4);
+
+  /************************** CONTEXT 3 **************************/  
+  BeliefTablePtr btab33(new BeliefTable);
+  //kbspec3 = ex;
+  //kbspec3 += "/context3.lp";
+  kbspec3 = "../../examples/context3.lp";
+
+  Belief belief_epsilon33(ctx_id3, "epsilon");
+  Belief belief_cc1c4_33(ctx_id3, "c(c1,c4)");
+  Belief belief_cc2c5_33(ctx_id3, "c(c2,c5)");
+  Belief belief_cc3c6_33(ctx_id3, "c(c3,c6)");
+
+  ID id_epsilon33 = btab33->storeAndGetID(belief_epsilon33);
+  ID id_cc1c4_33 = btab33->storeAndGetID(belief_cc1c4_33);
+  ID id_cc2c5_33 = btab33->storeAndGetID(belief_cc3c5_33);
+  ID id_cc3c6_33 = btab33->storeAndGetID(belief_cc4c6_33);
+
+  BeliefTablePtr btab53(new BeliefTable);
+  Belief belief_epsilon53(ctx_id5, "epsilon");
+  Belief belief_ec1c4_53(ctx_id5, "e(c1,c4)");
+  Belief belief_ec2c5_53(ctx_id5, "e(c2,c5)");
+  Belief belief_ec3c6_53(ctx_id5, "e(c3,c6)");
+  Belief belief_fc1c4_53(ctx_id5, "f(c1,c4)");
+  Belief belief_fc2c5_53(ctx_id5, "f(c2,c5)");
+  Belief belief_fc3c6_53(ctx_id5, "f(c3,c6)");
+
+  ID id_epsilon53 = ID(ID::MAINKIND_BELIEF | ctx_id5, 0);
+  ID id_ec1c4_53 = ID(ID::MAINKIND_BELIEF | ctx_id5, 4);
+  ID id_ec2c5_53 = ID(ID::MAINKIND_BELIEF | ctx_id5, 5);
+  ID id_ec3c6_53 = ID(ID::MAINKIND_BELIEF | ctx_id5, 6);
+  ID id_fc1c4_53 = ID(ID::MAINKIND_BELIEF | ctx_id5, 7);
+  ID id_fc2c5_53 = ID(ID::MAINKIND_BELIEF | ctx_id5, 8);
+  ID id_fc3c6_53 = ID(ID::MAINKIND_BELIEF | ctx_id5, 9);
+
+  BOOST_CHECK_EQUAL(id_epsilon53, id_epsilon55);
+  BOOST_CHECK_EQUAL(id_ec1c4_53, id_ec1c4_55);
+  BOOST_CHECK_EQUAL(id_ec2c5_53, id_ec2c5_55);
+  BOOST_CHECK_EQUAL(id_ec3c6_53, id_ec3c6_55);
+  BOOST_CHECK_EQUAL(id_fc1c4_53, id_fc1c4_55);
+  BOOST_CHECK_EQUAL(id_fc2c5_53, id_fc2c5_55);
+  BOOST_CHECK_EQUAL(id_fc3c6_53, id_fc3c6_55);
+
+  // add beliefs to btab53 using the new method
+  btab53->storeWithID(belief_epsilon53, id_epsilon53);
+  btab53->storeWithID(belief_ec1c4_53, id_ec1c4_53);
+  btab53->storeWithID(belief_ec2c5_53, id_ec2c5_53);
+  btab53->storeWithID(belief_ec3c6_53, id_ec3c6_53);
+  btab53->storeWithID(belief_fc1c4_53, id_fc1c4_53);
+  btab53->storeWithID(belief_fc2c5_53, id_fc2c5_53);
+  btab53->storeWithID(belief_fc3c6_53, id_fc3c6_53);
+
+  // bridge rules C5 --> C3
+  BridgeRuleTablePtr bridge_rules(new BridgeRuleTable);
+
+  // adding all ground instances of c(C,Y) :- (5:e(X,Y));
+  //
+  // c(c1,c4) :- (5:e(c1,c4)).
+  Tuple body13;
+  body13.push_back(id_ec1c4_53);
+  BridgeRule br13(ID::MAINKIND_RULE | ID::SUBKIND_RULE_BRIDGE_RULE, id_cc1c4_33, body13);
+  bridge_rules3->storeAndGetID(br13);
+
+  // c(c2,c5) :- (5:e(c2,c5)).
+  Tuple body23;
+  body23.push_back(id_ec2c5_53);
+  BridgeRule br23(ID::MAINKIND_RULE | ID::SUBKIND_RULE_BRIDGE_RULE, id_cc2c5_33, body23);
+  bridge_rules3->storeAndGetID(br23);
+
+  // c(c3,c6) :- (5:e(c3,c6)).
+  Tuple body33;
+  body33.push_back(id_ec3c6_53);
+  BridgeRule br33(ID::MAINKIND_RULE | ID::SUBKIND_RULE_BRIDGE_RULE, id_cc3c6_33, body33);
+  bridge_rules3->storeAndGetID(br33);
 }
 
 // Local Variables:
