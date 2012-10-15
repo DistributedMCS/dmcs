93976bb391e3aec8dcb92ec80facd53fbf733431
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2012-10-15 16:56:31 +0000

    Diamond+ example.

---
+++ examples/context.cmd
@@ -0,0 +1,11 @@
+./new_dmcsd --context=1 --port=5001 --system-size=5 --belief-state-size=10 --kb=../../examples/context1.lp --br=../../examples/context1.br --queryplan=../../examples/context1.qp
+
+./new_dmcsd --context=2 --port=5002 --system-size=5 --belief-state-size=10 --kb=../../examples/context2.lp --br=../../examples/context2.br --queryplan=../../examples/context2.qp
+
+./new_dmcsd --context=3 --port=5003 --system-size=5 --belief-state-size=10 --kb=../../examples/context3.lp --br=../../examples/context3.br --queryplan=../../examples/context3.qp
+
+./new_dmcsd --context=4 --port=5004 --system-size=5 --belief-state-size=10 --kb=../../examples/context4.lp --br=../../examples/context4.br --queryplan=../../examples/context4.qp
+
+./new_dmcsd --context=5 --port=5005 --system-size=5 --belief-state-size=10 --kb=../../examples/context5.lp --br=../../examples/context5.br --queryplan=../../examples/context5.qp
+
+./new_dmcsc --port=5001 --root=1 --k1=1 --k2=4
