85e07e052b5c938a91115a5a2c03af3bb537b7d3
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2012-10-18 15:28:02 +0000

    Shift context ids from 1-5 to 0-4.

--- examples/context.cmd
+++ examples/context.cmd
@@ -1,3 +1,5 @@
+./new_dmcsd --context=0 --port=5000 --system-size=0 --packsize=4 --belief-state-size=10 --kb=../../examples/context0.lp --br=../../examples/context0.br --queryplan=../../examples/context0.qp
+
 ./new_dmcsd --context=1 --port=5001 --system-size=5 --packsize=4 --belief-state-size=10 --kb=../../examples/context1.lp --br=../../examples/context1.br --queryplan=../../examples/context1.qp
 
 ./new_dmcsd --context=2 --port=5002 --system-size=5 --packsize=4 --belief-state-size=10 --kb=../../examples/context2.lp --br=../../examples/context2.br --queryplan=../../examples/context2.qp
@@ -6,6 +8,4 @@
 
 ./new_dmcsd --context=4 --port=5004 --system-size=5 --packsize=4 --belief-state-size=10 --kb=../../examples/context4.lp --br=../../examples/context4.br --queryplan=../../examples/context4.qp
 
-./new_dmcsd --context=5 --port=5005 --system-size=5 --packsize=4 --belief-state-size=10 --kb=../../examples/context5.lp --br=../../examples/context5.br --queryplan=../../examples/context5.qp
-
-./new_dmcsc --port=5001 --root=1 --k1=1 --k2=4
+./new_dmcsc --port=5000 --root=0 --k1=1 --k2=4
