58bbb4ac8886fbba2d9623717eb17fe125061f98
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2012-11-14 09:16:26 +0000

    Cleaning.

--- examples/pingpong/pingpong.cmd
+++
@@ -1,3 +0,0 @@
-./dmcsd --logging=../../log4cxx.cfg --context=1 --port=5001 --kb=tests/pingpong1.lp --br=tests/pingpong1.br --topology=tests/pingpong.opt
-./dmcsd --logging=../../log4cxx.cfg --context=2 --port=5002 --kb=tests/pingpong2.lp --br=tests/pingpong2.br --topology=tests/pingpong.opt
-time ./dmcsc --hostname=localhost --port=5001 --system-size=2
--- examples/pingpong/pingpong.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dmcs=${DMCS:=./src}
-log4cxx=${LOG4CXX:=.}
-examplesdir=${EXAMPLESDIR:=./examples}
-$dmcs/dmcsd --daemon --logging=$log4cxx/log4cxx.cfg --context=1 --port=5001 --kb=$examplesdir/pingpong1.lp --br=$examplesdir/pingpong1.br --topology=$examplesdir/pingpong.opt
-$dmcs/dmcsd --daemon --logging=$log4cxx/log4cxx.cfg --context=2 --port=5002 --kb=$examplesdir/pingpong2.lp --br=$examplesdir/pingpong2.br --topology=$examplesdir/pingpong.opt
-time $dmcs/dmcsc --hostname=localhost --port=5001 --system-size=2
