5b8cedee57e26f83dc31822dd1440f8ae4cdd892
mmsc <mmsc@9a859a62-f9af-11dd-b3bf-39592bdb26af>
2012-12-10 10:18:53 +0000

    Change output ordering.

--- examples/test/listTests.py
+++ examples/test/listTests.py
@@ -3,35 +3,46 @@
 import getopt, sys
 import optparse
 import string
+from optparse import OptionParser
 
 def main(argv):
-    toponames      = ['diamond', 'tree', 'zigzag', 'ring']
-    topo_ids       = [ 1, 6, 3, 4 ]
-    system_sizes   = [[4],  # diamond
-                      [4], # tree
-                      [4],  # zigzag
-                      [4]  # ring
-                     ]
+    parser = OptionParser()
+    parser.add_option("-e", "--ext", dest="extension",
+                      help="write configuration with extension EXT", metavar="EXT")
 
-    toponames      = ['diamond']
-    topo_ids       = [ 1, 6, 3, 4 ]
-    system_sizes   = [[4],  # diamond
-                      [4], # tree
-                      [4],  # zigzag
-                      [4]  # ring
-                      ]
+    (options, args) = parser.parse_args()
 
-    no_atoms       = [9 ]
-    no_interface   = [ 5 ]
-    no_bridgerules = [ 5 ]
-    instances      = [ 'a', 'b' ]
+    extension = options.extension
+
+    #toponames      = ['diamond', 'tree', 'zigzag', 'ring']
+    #topo_ids       = [ 1, 6, 3, 4 ]
+    #system_sizes   = [[4],  # diamond
+    #                  [4], # tree
+    #                  [4],  # zigzag
+    #                  [4]  # ring
+    #                 ]
+
+    toponames      = ['ring']
+    topo_ids       = [ 4 ]
+    system_sizes   = [[4, 7, 10, 13, 70]]
+
+    #toponames      = ['diamond']
+    #topo_ids       = [ 1 ]
+    #system_sizes   = [[4, 7, 10, 13, 25, 31]]
+
+    
+    no_atoms       = [ 10, 20 ]
+    no_interface   = [ 5,  10 ]
+    no_bridgerules = [ 5,  10 ]
     #instances      = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' ]
+    instances      = [ 'a', 'b', 'c', 'd', 'e' ]
+    
 
     for i in range(0,len(toponames)):
-        filename = 'config/' + toponames[i] + '.cfg'
+        filename = 'config/' + toponames[i] + '.' + extension
         with open(filename, 'w') as f:
-            for j in range(0,len(system_sizes[i])):
-                for k in range(0,len(no_atoms)):
+            for k in range(0,len(no_atoms)):
+                for j in range(0,len(system_sizes[i])):
                     for ins in range(0,len(instances)):
                         tmp = str(topo_ids[i]) + ',' + str(system_sizes[i][j]) + ',' + str(no_atoms[k]) + ',' + str(no_interface[k]) + ',' + str(no_bridgerules[k]) + ',' + instances[ins] + '\n'
                         f.write(tmp)
