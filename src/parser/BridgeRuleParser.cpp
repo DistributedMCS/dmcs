f21335d20464c1108d9c1d2474f24e67bbbb5b23
Peter Schueller <schueller.p@gmail.com>
2017-04-17 23:02:34 +0300

    more phoenix header fixes

--- src/parser/BridgeRuleParser.cpp
+++ src/parser/BridgeRuleParser.cpp
@@ -41,13 +41,14 @@
 
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_object.hpp>
-#include <boost/spirit/include/phoenix_fusion.hpp>
-#include <boost/spirit/include/phoenix_stl.hpp>
+//#include <boost/spirit/include/phoenix_core.hpp>
+//#include <boost/spirit/include/phoenix_operator.hpp>
+//#include <boost/spirit/include/phoenix_object.hpp>
+//#include <boost/spirit/include/phoenix_fusion.hpp>
+//#include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/spirit/include/phoenix.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
-#include <boost/spirit/home/phoenix/statement/if.hpp> 
+//#include <boost/spirit/home/phoenix/statement/if.hpp> 
 
 namespace {
 
